0f8d2bb8132c015f3eade425616a6b085ad727ac
Auden Hinton <auden@outlook.com>
2022-05-08 17:40:44 -0700

    Added operators to query endpoint

--- noto.js
+++ noto.js
@@ -199,38 +199,114 @@ class NotoDB {
 
     queryItems(req, res) {
 
+        let where_clause = ''
+
+        if(Object.keys(req.query).length > 0){
+            where_clause = " WHERE "
+
+            Object.keys(req.query).forEach((item, index) => {
+
+                let needle = req.query[item]
+                let operator = ` = '${needle}' `
+
+                // check to make sure we are comparing the correct type (string, int, float)
+                if(parseInt(needle)) operator = ` = ${needle} `
+                if(parseFloat(needle)) operator = ` = ${needle} `
+
+                // check for comparison operators
+                if(needle.indexOf(":") > -1){
+
+                    switch(needle.split(":")[0]){
+
+                        case "like":
+                            operator = ` LIKE '%${needle.split(":")[1]}%' `
+                            break;
+    
+                        case "start":
+                            operator = ` LIKE '${needle.split(":")[1]}%' `
+                            break;
+    
+                        case  "end":
+                            operator = ` LIKE '%${needle.split(":")[1]}' `
+                            break;
+    
+                        case "gt":
+                            operator = ` > ${needle.split(":")[1]} `
+                            break;
+    
+                        case  "lt":
+                            operator = ` < ${needle.split(":")[1]} `
+                            break;
+    
+                        case "gte":
+
+                            // these need validation so that users can only use these operators on int & float
+
+                            operator = ` >= '${needle.split(":")[1]}' `
+                            if(parseInt(needle)) operator = ` >= ${needle} `
+                            if(parseFloat(needle)) operator = ` >= ${needle} `
+
+                            break;
+    
+                        case "lte":
+
+                            operator = ` <= '${needle.split(":")[1]}' `
+                            if(parseInt(needle)) operator = ` <= ${needle} `
+                            if(parseFloat(needle)) operator = ` <= ${needle} `
+
+                            break;
+    
+                    }
+
+                }
+
+                where_clause = where_clause + ((index === 0) ? "" : "AND") + " s." + item + operator
+
+            });
+
+        }        
+
         const params = {
             Bucket: req.params.bucket,
             Key: "_notodb/" + req.params.set,
             ExpressionType: 'SQL',
-            Expression: "SELECT * FROM S3Object[*][*] s WHERE s.first_name LIKE 'blah' ",
-            InputSerialization: { 'JSON': { "Type": "Lines" } },
-            OutputSerialization: { 'JSON': {} }
+            Expression: "SELECT * FROM S3Object[*][*] s " + where_clause,
+            InputSerialization: {
+                JSON: {
+                    Type: 'DOCUMENT',
+                }
+            },
+            OutputSerialization: {
+                JSON: {
+                    RecordDelimiter: ','
+                }
+            }
         }
 
 
         this.s3.selectObjectContent(params, function (err, data) {
 
-            console.log(err)
-
             let records = []
 
             data.Payload.on('data', (event) => {
-                // There are multiple events in the eventStream, but all we 
-                // care about are Records events. If the event is a Records 
-                // event, there is data inside it
+
                 if (event.Records) {
                     records.push(event.Records.Payload);
                 }
-            })
-                .on('error', (err) => {
-                    reject(err);
-                })
-                .on('end', () => {
 
-                        res.send(records.toString())
+            }).on('error', (err) => {
 
-                });
+                console.log(err);
+
+            }).on('end', () => {
+
+                let results = Buffer.concat(records).toString('utf8');
+                results = results.replace(/\,$/, '');
+                results = `[${results}]`;
+
+                res.send(results)
+
+            });
 
         });
 
